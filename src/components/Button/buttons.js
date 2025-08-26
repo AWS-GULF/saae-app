8f4e08e19eebb062f47c5449f92d7b55575ed5b1
Abdelwahab Eladawy <abdelwahabeladawy1611@gmail.com>
2025-08-26 18:30:21 +0300

    fix

--- src/components/Button/buttons.js
+++ src/components/Button/buttons.js
@@ -14,7 +14,7 @@ export const RegularButtonWhiteBg = ({ href = "#", children }) => {
     <div className="flex justify-center items-center">
       <Link
         href={href}
-        className="cursor-pointer w-[235px] h-[64px] border-2 border-secondary rounded-2xl bg-white flex justify-center items-center"
+        className="cursor-pointer p-5 border-2 border-secondary rounded-2xl bg-white flex justify-center items-center"
       >
         <div className="flex items-center gap-3 lg:gap-4">
           <span className="text-xl font-medium text-secondary leading-none">
